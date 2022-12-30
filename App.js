84ed75d713e112d9ca04130107059573b1995d97
Ashok Kumar D <goldenashok@users.noreply.github.com>
2022-12-30 16:21:36 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Auth0Provider } from 'react-native-auth0';
+import config from './auth0-configuration';
 import Home from './Home';
 
 export default function App() {
   return (
-    <Auth0Provider domain="scalper.eu.auth0.com" clientId="p4SHfqB2YCYEX8Gjt4wGnTbd0SYwab0S">
+    Auth0Provider domain={config.domain} clientId={config.clientId}>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
